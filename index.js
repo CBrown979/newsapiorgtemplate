29d798b4f21d3c04c03b17ff7ccb90247495db4d
Candice Bianca Brown <candicebrown97@gmail.com>
2017-12-02 17:57:28 +0000

    completed newsAPI assignment

--- index.js
+++ index.js
@@ -4,25 +4,45 @@ $(document).ready(function(){
     $.ajax({
         method: "GET",
         url: "https://newsapi.org/v2/sources",
-        data: { category: "business", country: "us", language: "en", apiKey: "27dba2b2690d4914856961f7d895c081" },
+        data: { category: "technology", country: "us", language: "en", apiKey: APIKEY },
         success: function(data){
             if(data.status == "ok"){
+                console.log("start");
                 console.log(data);
                 for(var i=0; i<data.sources.length; i++){
                     var source=document.createElement("OPTION");
+                    source.setAttribute("value", data.sources[i].id);
                     source.innerHTML=data.sources[i].name;//attributes needed to display
                     document.getElementById('selection').appendChild(source);
                 }
+                console.log("stop");
             }
         }
-     })
-    //  .done(function(data) {
-    //      console.log( data );
-    //      console.log(data.status);
-    //     //alert( "Data Saved: " + msg );
-    //  });
-    $('#source').submit(function(event){
-        event.preventDefault();
-        alert(document.getElementById("selection").value)
-    })
-})
+    });
+});
+    document.getElementById("getHeadlines").onclick = function(event){
+	  event.preventDefault();
+	  document.getElementById("headlines").innerHTML="";
+        var articleHeads=document.getElementById("selection").value;
+        $.ajax({
+            method: "GET",
+            url: "https://newsapi.org/v2/top-headlines",
+            data: {country: "us", language: "en", sources: articleHeads, apiKey: APIKEY},
+            success: function(data){
+                if(data.status == "ok"){
+                    console.log("begin");
+                    //document.getElementById("quoteInfo").reset();
+                    console.log(data.articles[0].title);
+                    for(var i=0; i<data.articles.length; i++){
+                        var newHeadline=document.createElement("li");
+                        //document.getElementById("headlines").reset();
+                        newHeadline.innerHTML=data.articles[i].title;
+                        document.getElementById('headlines').appendChild(newHeadline);
+                        
+                    }
+                    console.log("finish");
+                }
+            }
+        });
+    };
+    
